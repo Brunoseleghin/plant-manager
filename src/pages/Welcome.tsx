e1d58812105becae81b18102dea869e27da0e127
Bruno Seleghin <brunoseleghin@gmail.com>
2022-03-28 23:35:06 -0300

    Alter Welcome page

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -1,60 +1,98 @@
-import React, { useState } from 'react';
-import { SafeAreaView, Text, Image, StyleSheet } from 'react-native'
-import wateringImg from '../assets/watering.png'
+import React from 'react';
+import {
+  SafeAreaView,
+  Text,
+  Image,
+  StyleSheet,
+  TouchableOpacity,
+  Dimensions,
+  View
+} from 'react-native';
+import { Entypo } from '@expo/vector-icons'
+import { StatusBar } from 'expo-status-bar';
+
+import wateringImg from '../assets/watering.png';
 import colors from '../styles/colors';
-import { Button } from '../components/Button';
+import fonts from '../styles/fonts';
+
 
 export function Welcome(){
-  const [visible, setVisible] = useState(false)
-
-  function handleVisibility(){
-    setVisible(true)
-  }
-
   return (
     <SafeAreaView style={styles.container}>
-      <Text style={styles.title}>
-        Gerencie {'\n'}
-        suas plantas {'\n'}
-        de forma fácil
-      </Text>
+      <View style={styles.wrapper}>
+        <Text style={styles.title}>
+          Gerencie {'\n'}
+          suas plantas de {'\n'}
+          forma fácil
+        </Text>
 
-      {
-        visible &&
-        <Image source={wateringImg} style={styles.image} />
-      }
+        <Image
+          source={wateringImg}
+          style={styles.image}
+          resizeMode="contain"
+        />
 
-      <Text style={styles.subtitle}>
-          Não esqueça mais de regar suas plantas.
-          Nós cuidamos de lembrar você sempre que precisar.
-      </Text>
+        <Text style={styles.subtitle}>
+            Não esqueça mais de regar suas {'\n'}
+            plantas. Nós cuidamos de lembrar você {'\n'}
+            sempre que precisar.
+        </Text>
 
-      <Button title='>' onPress={handleVisibility}/>
+        <TouchableOpacity
+          style={styles.button}
+          activeOpacity={0.6}
+        >
+          <Entypo 
+            name="chevron-right"
+            style={styles.buttonIcon}
+          />
+        </TouchableOpacity>
+      </View>
+      <StatusBar style='auto'/>
     </SafeAreaView>
   )
 }
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1
+  },
+  wrapper: {
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'space-between'
+    justifyContent: 'space-around',
+    paddingHorizontal: 20
   },
   title: {
-    fontSize: 32,
+    fontSize: 28,
     fontWeight: 'bold',
     textAlign: 'center',
     color: colors.heading,
-    marginTop: 38
+    marginTop: 38,
+    fontFamily: fonts.heading,
+    lineHeight: 34
   },
   image: {
-    width: 292,
-    height: 284
+    height: Dimensions.get('window').width * 0.7
   },
   subtitle: {
     textAlign: 'center',
     fontSize: 18,
     paddingHorizontal: 20,
-    color: colors.body_dark
+    color: colors.body_dark,
+    fontFamily: fonts.text
+  },
+  button: {
+    backgroundColor: colors.green,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 16,
+    marginBottom: 10,
+    height: 56,
+    width: 56
+  },
+  buttonIcon: {
+    color: colors.white,
+    fontSize: 32
   }
 });
