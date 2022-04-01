72185ade27cb9b9915dba3a2f4315d8d96710af7
Bruno Seleghin <brunoseleghin@gmail.com>
2022-03-31 21:18:07 -0300

    Create Page Confirmation

---
+++ src/pages/Confirmation.tsx
@@ -0,0 +1,76 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native'
+import { Button } from '../components/Button';
+import colors from '../styles/colors';
+import fonts from '../styles/fonts';
+
+export function Confirmation(){
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.content}>
+        <Text style={styles.emoji}>
+          😁
+        </Text>
+        
+        <Text style={styles.title}>
+          Prontinho
+        </Text>
+
+        <Text style={styles.subtitle}>
+          Agora vamos começar a cuidar das suas {'\n'}
+          plantinhas com muito cuidado.
+        </Text>
+
+        <View style={styles.footer}>
+          <Button title='Começar'/>
+        </View>
+        
+      </View>
+      <StatusBar style='auto'/>
+    </SafeAreaView>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'space-around'
+  },
+  content: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '100%',
+    padding: 30
+  },
+  emoji: {
+    fontSize: 96,
+    marginBottom: 64
+  },
+  title: {
+    fontSize: 24,
+    fontFamily: fonts.heading,
+    textAlign: 'center',
+    color: colors.heading,
+    lineHeight: 38,
+    marginBottom: 16
+  },
+  subtitle: {
+    fontFamily: fonts.text,
+    textAlign: 'center',
+    fontSize: 17,
+    color: colors.heading,
+    marginBottom: 40
+  },
+  footer: {
+    width: '100%',
+    paddingHorizontal: 75,
+  }
+})
