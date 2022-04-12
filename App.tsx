60281d32ac44a5ce58503c51d7dd57d49520b817
Bruno Seleghin <brunoseleghin@gmail.com>
2022-04-11 21:10:50 -0300

    Add ignoreLogs

--- App.tsx
+++ App.tsx
@@ -8,6 +8,12 @@ import {
 
 import Routes from './src/routes';
 
+import { LogBox } from 'react-native';
+
+LogBox.ignoreLogs([
+  "[react-native-gesture-handler] Seems like you're using an old API with gesture components, check out new Gestures system!",
+]);
+
 export default function App(){
 
   const [ fontsLoaded ] = useFonts({
