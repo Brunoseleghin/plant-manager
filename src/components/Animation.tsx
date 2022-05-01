f1b4ac9acda56ba7ab348f56bbbdc6453f852eb4
Bruno Seleghin <brunoseleghin@gmail.com>
2022-05-01 20:12:46 -0300

    Add component Animation

---
+++ src/components/Animation.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import LottieView from 'lottie-react-native';
+
+export function Animation({loadAnimation}: any){
+  return (
+    <View style={styles.container}>
+      <LottieView
+        source={loadAnimation}
+        autoPlay
+        loop
+        style={styles.animation}
+      />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  animation: {
+      backgroundColor: 'transparent',
+      width: 250,
+      height:250
+  }
+})
